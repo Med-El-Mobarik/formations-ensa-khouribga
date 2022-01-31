c0e24c765f88686361d0fed055d30d52c9e791b0
med el mobarik <simoxmbk1@gmail.com>
2022-01-31 17:38:04 +0100

    second commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    HOST_SERVER: "sql11.freemysqlhosting.net",
+    DB_NAME: "sql11468570",
+    DB_USER: "sql11468570",
+    DB_PASSWORD: "9dL7AaMrWc",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
