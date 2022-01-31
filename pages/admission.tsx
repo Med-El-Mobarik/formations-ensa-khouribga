21ff5d7eb468a1011a9c6a1f1640054897475418
med el mobarik <simoxmbk1@gmail.com>
2022-01-31 18:34:24 +0100

    second commit

--- pages/admission.tsx
+++ pages/admission.tsx
@@ -1,14 +1,7 @@
-import { GetServerSideProps } from "next";
 import Adm from "../components/admission";
 
 const Admission = () => {
   return <Adm />;
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    props: {},
-  };
 };
 
 export default Admission;
