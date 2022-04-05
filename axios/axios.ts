121ff7c8e2b476eebc8ff7c23309efba53acec84
med el mobarik <simoxmbk1@gmail.com>
2022-04-05 18:27:24 +0000

    blabla

--- axios/axios.ts
+++ axios/axios.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:3000/api/",
-});
-
 // const instance = axios.create({
-//   baseURL: "https://ensakh-formations.vercel.app/api/",
+//   baseURL: "http://localhost:3000/api/",
 // });
+
+const instance = axios.create({
+  baseURL: "https://ensakh-formations.vercel.app/api/",
+});
 
 export default instance;
